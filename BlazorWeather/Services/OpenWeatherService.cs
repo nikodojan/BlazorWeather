f3cd522c010d9e1bbe27c1c5c2f63363696e2f73
Niko D <nikodojan@live.de>
2022-05-19 16:06:51 +0200

    geolocation works

--- BlazorWeather/Services/OpenWeatherService.cs
+++ BlazorWeather/Services/OpenWeatherService.cs
@@ -1,21 +1,31 @@
 ﻿using BlazorWeather.Models;
+using Microsoft.JSInterop;
 using System.Net.Http.Json;
+using System.Xml.Linq;
 
 namespace BlazorWeather.Services
 {
     public class OpenWeatherService
     {
         private const string ApiKey = "629c2330ea97fc0152bdfc4100f0c0ac";
-        private HttpClient _http;
+        private HttpClient _http;      
 
         public OpenWeatherService(HttpClient http)
         {
             _http = http;
         }
 
-        public async Task<WeatherApiResponse> GetWeather(string name)
+        public async Task<WeatherApiResponse> GetWeatherByName(string name)
         {
             var response = await _http.GetAsync($"https://api.openweathermap.org/data/2.5/weather?q={name}&appid={ApiKey}&units=metric");
+            var weather = await response.Content.ReadFromJsonAsync<WeatherApiResponse>();
+
+            return weather;
+        }
+
+        public async Task<WeatherApiResponse> GetWeatherByCoords(string lat, string lon)
+{
+            var response = await _http.GetAsync($"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={ApiKey}&units=metric");
             var weather = await response.Content.ReadFromJsonAsync<WeatherApiResponse>();
 
             return weather;
---
+++ BlazorWeather/wwwroot/js/main.js
@@ -0,0 +1,31 @@
+﻿/**
+ * Wrap function in a Promise.
+ * Resolve when position is found.
+ * Reject when error occurs.
+ */
+const getLocation = () => new Promise(
+    (resolve, reject) => {
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                const location = {
+                    lat: position.coords.latitude,
+                    lon: position.coords.longitude
+                };
+                resolve(location); // Resolve with location. location can now be accessed in the .then method.
+            }, 
+            err => resolve({ lat: 55.6733995892128, lon: 12.565123825679906 }) // Reject with err. err can now be accessed in the .catch method.
+        );
+    }
+);
+
+/**
+ * then is called when the Promise is resolved
+ * catch is called when the Promise rejects or encounters an error.
+ */
+getLocation()
+    .then(location => { return location });
+
+async function getloc() {
+    var p = await getLocation();
+    return p;
+}
